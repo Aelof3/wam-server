a426dd0a0e1a006ab02d71ad354f2fde4cf02e61
Aelof3 <folea3@gmail.com>
2020-10-20 04:56:51 -0400

    test2

--- src/config.js
+++ src/config.js
@@ -4,4 +4,5 @@ module.exports = {
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://wam_admin:wam_pass@localhost/wam_db",
   DATABASE_URL: process.env.DATABASE_URL || "postgresql://wam_admin:wam_pass@localhost/wam_db"
+  //DATABASE_URL: process.env.DATABASE_URL || "postgres://grfdpibyfjadyc:647a2c8f7aa88937044ff7fa12124054b9e259dc3d074c65bb1a8efc49463aab@ec2-52-3-4-232.compute-1.amazonaws.com:5432/d1vppgo6jjurh"
 }
